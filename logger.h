e29ce61b1f9369a0f05814b1ec0e699a02f8382b
adamhzp <kod.adam.ho@gmail.com>
2017-01-08 13:02:38 -0500

    add logger

---
+++ logger.h
@@ -0,0 +1,63 @@
+/*
+ * A simple Logger for c programs
+ * @Author Adam Ho
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct logger{
+	
+	FILE* fd; //file descriptor
+	char mode; // 'w' for overwriting, 'a' for appending
+
+}logger_t;
+
+void init_logger(logger_t *logger, char mode, char *path)
+{
+	if(logger == NULL || (mode != 'w' && mode !='a') || path == NULL) return;
+
+	switch(mode){
+		case 'w':
+			logger->fd = fopen(path, "w");
+			break;
+		case 'a':
+			logger->fd = fopen(path, "a");
+			break;
+		default:
+			printf("something wrong\n");
+			break;
+	}
+
+	logger->mode = mode;
+	if(logger->fd == NULL) printf("Failed to open %s to write.\n", path);
+}
+
+void log_to_file(logger_t *logger, char *str, int len){
+	if(logger == NULL) return;
+	//printf("str: %s|len:%d\n",str,len);
+	fwrite(str, len, 1, logger->fd);
+}
+
+void free_logger(logger_t *logger)
+{
+	if(logger == NULL) return;
+
+	fclose(logger->fd);
+	free(logger);
+}
+
+
+void write_to_file(char *filepath, char mode, char *buf, int len)
+{
+	logger_t *logger;
+	if(buf == NULL || filepath == NULL) return;
+
+	logger = (logger_t*) malloc(sizeof(logger_t));
+	init_logger(logger, mode, filepath);
+
+	log_to_file(logger, buf, len);
+
+	free_logger(logger);
+}
+
+
