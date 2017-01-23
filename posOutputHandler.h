8bd7f1ae528884e8a8ea0b5d3fa7b62a4de5f5fc
adamhzp <kod.adam.ho@gmail.com>
2017-01-23 15:57:47 -0500

    setup output

--- posOutputHandler.h
+++ posOutputHandler.h
@@ -7,6 +7,8 @@
 
 #include "position.h"	//agent handler, rssi_q are here...  need to be restructure to make it look nice..
 
+static const char *s_http_port = "8000";
+static struct mg_serve_http_opts s_http_server_opts;
 
 void handle_output_connection(pos_list_t *pos_list, int connfd){
 	int len, offset=0;
@@ -29,6 +31,53 @@ void handle_output_connection(pos_list_t *pos_list, int connfd){
 		}
 	}
 }
+
+static void ev_handler(struct mg_connection *c, int ev, void *p) {
+
+	if (ev == MG_EV_HTTP_REQUEST) {
+		struct http_message *hm = (struct http_message *) p;
+		char url[100];
+
+		snprintf(url, sizeof(url), "%.*s\n", (int) hm->uri.len, hm->uri.p);
+
+		if(strcmp(url, "/test") == 0){
+			mg_printf(c, 
+				"HTTP/1.1 200 OK\r\n"
+				"Content-Type: application/json\r\n"
+				"Content-Length: %d\r\n"
+				"\r\n"
+				"%s",
+				(int) strlen(url), url);
+				return;
+		}
+
+	}
+}
+
+void start_web_server() {
+	struct mg_mgr mgr;
+	struct mg_connection *nc;
+
+	mg_mgr_init(&mgr, NULL);
+	printf("Starting web server on port %s\n", s_http_port);
+	nc = mg_bind(&mgr, s_http_port, ev_handler);
+	if (nc == NULL) {
+		printf("Failed to create listener\n");
+		return 1;
+	}
+	// Set up HTTP server parameters
+	mg_set_protocol_http_websocket(nc);
+	s_http_server_opts.document_root = ".";  // Serve current directory
+	s_http_server_opts.enable_directory_listing = "yes";
+
+	for (;;) {
+		mg_mgr_poll(&mgr, 1000);
+	}
+	mg_mgr_free(&mgr);
+	return 0;
+}
+
+
 
 
 void *start_pos_output_thread(void *arg){
